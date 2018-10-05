db5b6ceb0fc8facbd0a821b263c27e9959439773
noritake_msak <noritake_msak@actis.co.jp>
2018-10-05 10:56:54 +0900

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(void){
+    printf("Hello Masa\n");
+    printf("%d\n", 100);
+    printf("");
+
+    printf("%d+", 100);
+    printf("%d=", 200);
+    printf("%d\n", 300);
+    
+    printf("%d+%d=%d\n", 100,200,300);
+    printf("%d\n", 100+500);
+    
+    printf("%d/%d=\n", 40, 13);
+    printf("%d\n", 40/13);
+    printf("%d\n", 40%13);
+
+    int value;
+    value = 10;
+    value += 30;
+    printf("%d\n", value);
+    printf("%f\n", 1.09 *9);
+    int a = 1000,b=500. c=3;
+    printf)(a = 10000, b=256.)
+
+    return 0;
+}
